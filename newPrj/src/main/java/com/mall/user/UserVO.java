039b26dd2da7261ee8c3ac49500c8d42138040f6
dsa00340 <dsa00340@naver.com>
2022-03-04 15:32:07 +0900

    commit VO create

---
+++ newPrj/src/main/java/com/mall/user/UserVO.java
@@ -0,0 +1,65 @@
+package com.mall.user;
+
+public class UserVO {
+      private String userId;
+      private String pwd;
+      private String tel;
+      private String address;
+      private String isWithRaw;
+      private String author;
+      private String regDate;
+      private String userName;
+      
+      public String getUserId() {
+         return userId;
+      }
+      public void setUserId(String userId) {
+         this.userId = userId;
+      }
+      public String getPwd() {
+         return pwd;
+      }
+      public void setPwd(String pwd) {
+         this.pwd = pwd;
+      }
+      public String getTel() {
+         return tel;
+      }
+      public void setTel(String tel) {
+         this.tel = tel;
+      }
+      public String getAddress() {
+         return address;
+      }
+      public void setAddress(String address) {
+         this.address = address;
+      }
+      public String getIsWithRaw() {
+         return isWithRaw;
+      }
+      public void setIsWithRaw(String isWithRaw) {
+         this.isWithRaw = isWithRaw;
+      }
+      public String getAuthor() {
+         return author;
+      }
+      public void setAuthor(String author) {
+         this.author = author;
+      }
+      public String getRegDate() {
+         return regDate;
+      }
+      public void setRegDate(String regDate) {
+         this.regDate = regDate;
+      }
+      public String getUserName() {
+         return userName;
+      }
+      public void setUserName(String userName) {
+         this.userName = userName;
+      }
+      
+   
+      
+}
+      
